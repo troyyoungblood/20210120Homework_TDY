ca486c31666e156f3c4525dcffe5076ba1cdb208
troyyoungblood <72400175+troyyoungblood@users.noreply.github.com>
2021-01-14 11:13:12 -0600

    table schemata and queries

---
+++ SQL_Challenge/EmployeeSQL/data/ty_sqlchallenge_queries.sql
@@ -0,0 +1,75 @@
+-- DATA ANALYSIS
+
+-- 1. List the following details of each employee: employee number, 
+--    last name, first name, sex, and salary.
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, salary.salary
+FROM employee AS e
+RIGHT OUTER JOIN salary
+ON e.emp_no = salary.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT e.first_name, e.last_name, e.hire_date
+FROM employee AS e
+WHERE hire_date BETWEEN '12-31-1985' AND '1-1-1987';
+
+-- 3. List the manager of each department with the following information: 
+--    department number, department name, the manager's employee number, 
+--    last name, first name.
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+ RIGHT OUTER JOIN department AS d
+ ON dm.dept_no = d.dept_no
+  LEFT OUTER JOIN employee AS e
+  ON dm.emp_no = e.emp_no;
+
+-- 4. List the department of each employee with the following information: 
+--    employee number, last name, first name, and department name.
+
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+ RIGHT OUTER JOIN employee AS e
+ ON de.emp_no = e.emp_no
+  LEFT OUTER JOIN department AS d
+  ON de.dept_no = d.dept_no;
+
+-- 5. List first name, last name, and sex for employees whose first name 
+--    is "Hercules" and last names begin with "B."
+--    Using ILIKE because it is case independent
+
+SELECT e.first_name, e.last_name, e.sex
+FROM employee AS e
+WHERE first_name ILIKE 'Hercules' AND last_name ILIKE 'B%';
+
+-- 6. List all employees in the Sales department, 
+--    including their employee number, last name, first name, and department name.
+
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+ LEFT OUTER JOIN employee AS e
+ ON de.emp_no = e.emp_no
+  RIGHT OUTER JOIN department AS d
+  ON de.dept_no = d.dept_no
+  WHERE dept_name LIKE 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, 
+--    including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+ LEFT OUTER JOIN employee AS e
+ ON de.emp_no = e.emp_no
+  RIGHT OUTER JOIN department AS d
+  ON de.dept_no = d.dept_no
+  WHERE dept_name LIKE 'Sales' OR dept_name LIKE 'Development';
+
+-- 8. In descending order, list the frequency count of employee last names, 
+--    i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) AS "count"
+FROM employee
+GROUP BY last_name
+ORDER BY "count" DESC;
+
---
+++ SQL_Challenge/EmployeeSQL/data/ty_sqlchallenge_tableschemata.sql
@@ -0,0 +1,79 @@
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS salary;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS title;
+
+-- DATA ENGINEERING
+
+-- Use the information you have to create a table schema 
+-- for each of the six CSV files. Remember to specify data types, 
+-- primary keys, foreign keys, and other constraints.
+
+--  * For the primary keys check to see if the column is unique, 
+--    otherwise create a [composite key](https://en.wikipedia.org/wiki/Compound_key). 
+--    Which takes two primary keys in order to uniquely identify a row.
+--  * Be sure to create tables in the correct order to handle foreign keys.
+
+-- Import each CSV file into the corresponding SQL table. 
+-- **Note** be sure to import the data in the same order that 
+--          the tables were created and account for the headers when 
+--          importing to avoid errors.
+
+-- Tables need to be created in this order 
+-- because of Foreign Key dependicies
+-- also - csv imported in this order
+
+CREATE TABLE department (
+	dept_no VARCHAR(15) Primary Key,
+	dept_name VARCHAR(45) NOT NULL	
+);
+
+SELECT * FROM department;
+
+CREATE TABLE title(
+	title_id VARCHAR(10) PRIMARY KEY,
+	title VARCHAR(45)
+);
+
+SELECT * FROM title;
+
+CREATE TABLE employee (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,	
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(45) NOT NULL,
+	last_name VARCHAR(45) NOT NULL,
+	sex VARCHAR(10) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id ) REFERENCES title(title_id)
+);
+
+SELECT * FROM employee;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(15) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES department(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employee(emp_no)
+);
+
+SELECT * FROM dept_manager;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(15) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employee(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES department(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE salary(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employee(emp_no)
+);
+
+SELECT * FROM salary;
